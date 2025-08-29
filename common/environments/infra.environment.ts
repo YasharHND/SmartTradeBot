3b44597f57142b41d559f331499166b113387a1e
YasharHND <yashar.hnd.65@gmail.com>
2025-08-29 22:41:33 +0400

    chore

--- common/environments/infra.environment.ts
+++ common/environments/infra.environment.ts
@@ -1,5 +1,7 @@
 // Required
+export const PROJECT_NAME = 'PROJECT_NAME';
+
 export const AWS_ACCOUNT_ID = 'AWS_ACCOUNT_ID';
 export const AWS_REGION = 'AWS_REGION';
+
 export const ENVIRONMENT = 'ENVIRONMENT';
-export const PROJECT_NAME = 'PROJECT_NAME';
